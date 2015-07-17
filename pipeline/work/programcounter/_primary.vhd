ce7f5c486766625d8a41a68ee10eece89533d9bf
whx2015 <whx19952014@outlook.com>
2015-07-17 18:35:38 +0800

    programcounter
    programcounter.v

---
+++ pipeline/work/programcounter/_primary.vhd
@@ -0,0 +1,14 @@
+library verilog;
+use verilog.vl_types.all;
+entity programcounter is
+    port(
+        clk             : in     vl_logic;
+        PCSrc           : in     vl_logic_vector(2 downto 0);
+        ALUOut          : in     vl_logic;
+        ConBA           : in     vl_logic_vector(31 downto 0);
+        JT              : in     vl_logic_vector(31 downto 0);
+        DatabusA        : in     vl_logic_vector(31 downto 0);
+        PC              : out    vl_logic_vector(31 downto 0);
+        PCplusout       : out    vl_logic_vector(31 downto 0)
+    );
+end programcounter;
