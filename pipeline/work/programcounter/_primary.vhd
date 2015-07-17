8f44f7780515e66b650ff609c6d102bd103bed47
whx2015 <whx19952014@outlook.com>
2015-07-17 19:31:58 +0800

    fix some mistakes
    modify programcounter.v

--- pipeline/work/programcounter/_primary.vhd
+++ pipeline/work/programcounter/_primary.vhd
@@ -3,10 +3,11 @@ use verilog.vl_types.all;
 entity programcounter is
     port(
         clk             : in     vl_logic;
+        reset           : in     vl_logic;
         PCSrc           : in     vl_logic_vector(2 downto 0);
         ALUOut          : in     vl_logic;
         ConBA           : in     vl_logic_vector(31 downto 0);
-        JT              : in     vl_logic_vector(31 downto 0);
+        JT              : in     vl_logic_vector(25 downto 0);
         DatabusA        : in     vl_logic_vector(31 downto 0);
         PC              : out    vl_logic_vector(31 downto 0);
         PCplusout       : out    vl_logic_vector(31 downto 0)
